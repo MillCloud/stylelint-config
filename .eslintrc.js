437966b8450a5dfdf4de654b17fabf5575ab4627
ModyQyW <wurui7@mail3.sysu.edu.cn>
2020-11-12 18:41:09 +0800

    update deps

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  parser: 'babel-eslint',
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-};
--- prettier.config.js
+++
@@ -1,5 +0,0 @@
-const config = require('@modyqyw/prettier-config');
-
-module.exports = {
-  ...config,
-};
